8819c2f0972a5d7aa418018bb50da0aed53ba2a7
EXT02D25621 <artsiom.trafimiuk@consultant.turkcell.com.tr>
2023-02-08 17:22:45 +0000

    add ReactRefreshWebpackPlugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,9 +2,10 @@ import { BuildOptions } from './types/config';
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
 
 export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
-  return [
+  const plugins = [
     new HtmlWebpackPlugin({
       template: paths.html
     }),
@@ -16,6 +17,12 @@ export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPlu
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev)
     }),
-    new webpack.HotModuleReplacementPlugin()
-  ]
+  ];
+
+  if (isDev) {
+    plugins.push(new ReactRefreshWebpackPlugin())
+    plugins.push(new webpack.HotModuleReplacementPlugin())
+  }
+
+  return plugins;
 }
