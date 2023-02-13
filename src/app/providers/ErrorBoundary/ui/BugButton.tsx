20a785d035ba1017fc5a9f825e6c6382b070aa60
EXT02D25621 <artsiom.trafimiuk@consultant.turkcell.com.tr>
2023-02-13 12:47:00 +0000

    add 404 routes and component; add page loader; add error boundary

---
+++ src/app/providers/ErrorBoundary/ui/BugButton.tsx
@@ -0,0 +1,25 @@
+import { Button } from "shared/ui/Button/Button";
+import { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
+
+// Component for testing ErrorBoundary Component
+export const BugButton = () => {
+  const [error, setError] = useState(false);
+  const { t } = useTranslation();
+
+  const onThrow = () => setError(true);
+
+  useEffect(() => {
+    if (error) {
+      throw new Error('Error')
+    }
+  }, [error])
+
+  return (
+    <Button
+      onClick={onThrow}
+    >
+      { t('throwError')}
+    </Button>
+  );
+};
