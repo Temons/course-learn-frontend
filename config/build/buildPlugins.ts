e83702c8fc1edb6d6e037e5d6a35bc72ad410a1f
EXT02D25621 <artsiom.trafimiuk@consultant.turkcell.com.tr>
2023-02-16 12:36:49 +0000

    remove ReactRefreshWebpackPlugin; add translation to Sidebar

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,7 +2,6 @@ import { BuildOptions } from './types/config';
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
-import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
 import { BundleAnalyzerPlugin }  from "webpack-bundle-analyzer";
 
 export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
@@ -21,7 +20,6 @@ export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPlu
   ];
 
   if (isDev) {
-    plugins.push(new ReactRefreshWebpackPlugin({ overlay: false }))
     plugins.push(new webpack.HotModuleReplacementPlugin())
     plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: false }))
   }
