f8ee0d59e4889965141d3355612543740a9a7291
EXT02D25621 <artsiom.trafimiuk@consultant.turkcell.com.tr>
2023-02-28 12:12:07 +0000

    fix unusual reducer prop type for Store Decorator

--- src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
@@ -8,7 +8,7 @@ const defaultAsyncReducers: DeepPartial<ReducersMapObject<StateSchema>> = {
 }
 export const StoreDecorator = (
   state: DeepPartial<StateSchema>,
-  asyncReducers: DeepPartial<ReducersMapObject<StateSchema>>
+  asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>
 // eslint-disable-next-line react/display-name
 ) => (StoryComponent: Story) => (
   <StoreProvider initialState={state} asyncReducers={{ ...defaultAsyncReducers, ...asyncReducers }}>
