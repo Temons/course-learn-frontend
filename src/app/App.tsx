486328fb2e19993b2dca2ba26823487c0b3f316e
EXT02D25621 <artsiom.trafimiuk@consultant.turkcell.com.tr>
2023-03-13 13:08:24 +0000

    update app router; changed runs-on: ubuntu-latest; add favicon

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,23 +3,26 @@ import { AppRouter } from "app/providers/router";
 import { Navbar } from "widgets/Navbar";
 import { Sidebar } from "widgets/Sidebar";
 import { Suspense, useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { userActions } from "entities/User";
+import { useDispatch, useSelector } from "react-redux";
+import { getUserInited, userActions } from "entities/User";
+import { useTheme } from "app/providers/ThemeProvider";
 
 const App = () => {
+  const { theme } = useTheme();
   const dispatch = useDispatch();
+  const inited = useSelector(getUserInited);
 
   useEffect(() => {
     dispatch(userActions.initAuthData())
   }, [dispatch])
 
   return (
-    <div className={classNames("app", {}, [])}>
+    <div className={classNames("app", {}, [theme])}>
       <Suspense fallback=''>
         <Navbar />
         <div className='content-page'>
           <Sidebar />
-          <AppRouter />
+          {inited && <AppRouter/>}
         </div>
       </Suspense>
 
