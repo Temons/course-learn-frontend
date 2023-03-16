59b41368b491258764ba1d19b1bee615f8ea1c09
EXT02D25621 <artsiom.trafimiuk@consultant.turkcell.com.tr>
2023-03-16 16:25:16 +0000

    remove loki references folder, update

--- src/shared/ui/Skeleton/Skeleton.stories.tsx
+++ src/shared/ui/Skeleton/Skeleton.stories.tsx
@@ -32,7 +32,7 @@ NormalDark.args = {
   width: '100%',
   height: 200
 };
-NormalDark.decorators = [ThemeDecorator(Theme.DARK)]
+NormalDark.decorators = [ThemeDecorator(Theme.DARK)];
 
 export const CircleDark = Template.bind({});
 CircleDark.args = {
@@ -40,4 +40,19 @@ CircleDark.args = {
   width: 100,
   height: 100
 };
-CircleDark.decorators = [ThemeDecorator(Theme.DARK)]
+CircleDark.decorators = [ThemeDecorator(Theme.DARK)];
+
+export const NormalOrange = Template.bind({});
+NormalOrange.args = {
+  width: '100%',
+  height: 200
+};
+NormalOrange.decorators = [ThemeDecorator(Theme.ORANGE)];
+
+export const CircleOrange = Template.bind({});
+CircleOrange.args = {
+  border: '50%',
+  width: 100,
+  height: 100
+};
+CircleOrange.decorators = [ThemeDecorator(Theme.ORANGE)];
