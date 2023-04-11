530cb532f1fa3a2b5a9a354e08f127b3c09d6648
EXT02D25621 <artsiom.trafimiuk@consultant.turkcell.com.tr>
2023-04-11 13:30:52 +0100

    add html reporter for unit tests

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -52,7 +52,15 @@ export default {
     '\\.(s?css$)': 'identity-obj-proxy',
     '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx')
   },
-
+  reporters: [
+    "default",
+    ["jest-html-reporters", {
+      publicPath: "<rootDir>/reports/unit",
+      filename: "report.html",
+      openReport: true,
+      inlineSource: true
+    }]
+  ]
   // All imported modules in your tests should be mocked automatically
   // automock: false,
 
