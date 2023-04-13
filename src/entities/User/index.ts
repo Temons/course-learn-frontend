d051a19a9f0c35cea3762c37648093e3d05e5720
EXT02D25621 <artsiom.trafimiuk@consultant.turkcell.com.tr>
2023-04-13 10:01:39 +0100

    add private route with required roles

--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -1,4 +1,5 @@
 export { getUserAuthData } from "./model/selectors/getUserAuthData/getUserAuthData";
 export { getUserInited } from "./model/selectors/getUserInited/getUserInited";
 export { userReducer, userActions } from './model/slice/userSlice';
-export { User, UserSchema } from './model/types/user'
+export { User, UserSchema, UserRole } from './model/types/user';
+export { isUserAdmin, getUserRoles, isUserManager } from './model/selectors/roleSelectors';
