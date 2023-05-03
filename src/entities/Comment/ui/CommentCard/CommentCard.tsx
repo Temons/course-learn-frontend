2a325082edc97d4dc99ca1fcc08063b3e7d8ce86
EXT02D25621 <artsiom.trafimiuk@consultant.turkcell.com.tr>
2023-05-03 14:56:23 +0100

    routing refactoring

--- src/entities/Comment/ui/CommentCard/CommentCard.tsx
+++ src/entities/Comment/ui/CommentCard/CommentCard.tsx
@@ -4,7 +4,7 @@ import { Comment } from "../../model/types/comment";
 
 import cls from './CommentCard.module.scss';
 
-import { RoutePath } from "@/shared/const/router";
+import { getRouteProfile } from "@/shared/const/router";
 import { classNames } from "@/shared/lib/classNames/classNames";
 import { AppLink } from "@/shared/ui/AppLink";
 import { Avatar } from "@/shared/ui/Avatar";
@@ -47,7 +47,7 @@ export const CommentCard = memo(({ className, comment, isLoading }: CommentCardP
       max
       className={classNames(cls.commentCard, {}, [className])}
     >
-      <AppLink to={`${RoutePath.profile}${comment.user.id}`} className={cls.header}>
+      <AppLink to={getRouteProfile(comment.user.id)} className={cls.header}>
         {comment.user.avatar ? <Avatar size={30} src={comment.user.avatar}/> : null}
         <Text className={cls.username} title={comment.user.username} />
       </AppLink>
