7e1b93c25f98d9f471da3320551e7a845b1eda12
EXT02D25621 <artsiom.trafimiuk@consultant.turkcell.com.tr>
2023-05-16 12:18:32 +0100

    update storybook, fix problems

--- src/features/articleRating/ui/ArticleRating/ArticleRating.stories.tsx
+++ src/features/articleRating/ui/ArticleRating/ArticleRating.stories.tsx
@@ -1,19 +1,15 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import React from 'react';
-import withMock from "storybook-addon-mock";
 
 import ArticleRating from "./ArticleRating";
 
 import { StoreDecorator } from "@/shared/config/storybook/StoreDecorator/StoreDecorator";
-
-
 export default {
   title: 'features/ArticleRating',
   component: ArticleRating,
   argTypes: {
     backgroundColor: { control: 'color' },
   },
-  decorators: [withMock]
 } as ComponentMeta<typeof ArticleRating>;
 
 const Template: ComponentStory<typeof ArticleRating> = (args) => <ArticleRating {...args} />;
--- src/features/articleRecommendationsList/ui/ArticleRecommendationsList/ArticleRecommendationsList.stories.tsx
+++ src/features/articleRecommendationsList/ui/ArticleRecommendationsList/ArticleRecommendationsList.stories.tsx
@@ -1,6 +1,5 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import React from 'react';
-import withMock from "storybook-addon-mock";
 
 import { ArticleRecommendationsList } from './ArticleRecommendationsList';
 
@@ -13,7 +12,6 @@ export default {
   argTypes: {
     backgroundColor: { control: 'color' },
   },
-  decorators: [withMock]
 } as ComponentMeta<typeof ArticleRecommendationsList>;
 
 const Template: ComponentStory<typeof ArticleRecommendationsList> = (args) => <ArticleRecommendationsList {...args} />;
