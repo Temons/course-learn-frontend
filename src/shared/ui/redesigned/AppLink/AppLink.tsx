e1880634b5d1026ad5b6b1afdae7df73b4b40b8f
EXT02D25621 <artsiom.trafimiuk@consultant.turkcell.com.tr>
2023-06-15 11:34:15 +0100

    redesign sidebar

---
+++ src/shared/ui/redesigned/AppLink/AppLink.tsx
@@ -0,0 +1,41 @@
+import { memo, ReactNode } from 'react';
+import { LinkProps, NavLink } from 'react-router-dom';
+
+import cls from './AppLink.module.scss';
+
+import { classNames } from '@/shared/lib/classNames/classNames';
+
+export type AppLinkVariant = 'primary' | 'red';
+
+interface AppLinkProps extends LinkProps {
+  className?: string;
+  variant?: AppLinkVariant;
+  children?: ReactNode;
+  activeClassName?: string;
+}
+
+export const AppLink = memo((props: AppLinkProps) => {
+  const {
+    to,
+    className,
+    children,
+    variant = 'primary',
+    activeClassName = '',
+    ...otherProps
+  } = props;
+
+  return (
+    <NavLink
+      to={to}
+      className={({ isActive }) =>
+        classNames(cls.AppLink, { [activeClassName]: isActive }, [
+          className,
+          cls[variant],
+        ])
+      }
+      {...otherProps}
+    >
+      {children}
+    </NavLink>
+  );
+});
