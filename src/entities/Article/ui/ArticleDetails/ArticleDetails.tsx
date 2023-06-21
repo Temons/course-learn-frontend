d87b21e2214b0858ab2e3cc5c662b497b2126473
EXT02D25621 <artsiom.trafimiuk@consultant.turkcell.com.tr>
2023-06-21 20:14:04 +0100

    redesign article details, fixes

--- src/entities/Article/ui/ArticleDetails/ArticleDetails.tsx
+++ src/entities/Article/ui/ArticleDetails/ArticleDetails.tsx
@@ -1,9 +1,7 @@
-import { memo, useCallback, useEffect } from 'react';
+import { memo, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 
-
-import { ArticleBlockType } from '../../model/consts/articlesConsts';
 import {
   getArticleDetailsData,
   getArticleDetailsError,
@@ -11,12 +9,9 @@ import {
 } from '../../model/selectors/articleDetails';
 import { fetchArticleById } from '../../model/services/fetchArticleById/fetchArticleById';
 import { articleDetailsReducer } from '../../model/slice/articleDetailsSlice';
-import { ArticleBlock } from '../../model/types/article';
-import { ArticleCodeBlockComponent } from '../ArticleCodeBlockComponent/ArticleCodeBlockComponent';
-import { ArticleImageBlockComponent } from '../ArticleImageBlockComponent/ArticleImageBlockComponent';
-import { ArticleTextBlockComponent } from '../ArticleTextBlockComponent/ArticleTextBlockComponent';
 
 import cls from './ArticleDetails.module.scss';
+import { renderArticleBlock } from './renderBlock';
 
 import CalendarIcon from '@/shared/assets/icons/calendar.svg';
 import EyeIcon from '@/shared/assets/icons/eye.svg';
@@ -25,12 +20,20 @@ import {
   DynamicModuleLoader,
   ReducersList,
 } from '@/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
+import { ToggleFeatures } from '@/shared/lib/features';
 import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch/useAppDispatch';
 import { Avatar } from '@/shared/ui/deprecated/Avatar';
 import { Icon } from '@/shared/ui/deprecated/Icon';
-import { Skeleton } from '@/shared/ui/deprecated/Skeleton';
-import { Text, TextAlign, TextSize } from '@/shared/ui/deprecated/Text';
+import { Skeleton as SkeletonDeprecated } from '@/shared/ui/deprecated/Skeleton';
+import {
+  Text as TextDeprecated,
+  TextAlign,
+  TextSize,
+} from '@/shared/ui/deprecated/Text';
+import { AppImage } from '@/shared/ui/redesigned/AppImage';
+import { Skeleton } from '@/shared/ui/redesigned/Skeleton';
 import { HStack, VStack } from '@/shared/ui/redesigned/Stack';
+import { Text } from '@/shared/ui/redesigned/Text';
 
 interface ArticleDetailsProps {
   className?: string;
@@ -41,25 +44,62 @@ const reducers: ReducersList = {
   articleDetails: articleDetailsReducer,
 };
 
+const Deprecated = () => {
+  const article = useSelector(getArticleDetailsData);
+
+  return (
+    <>
+      <HStack justify={'center'} max>
+        <Avatar size={200} src={article?.img} className={cls.avatar} />
+      </HStack>
+
+      <VStack gap={'4'} max data-testid={'ArticleDetails.Info'}>
+        <TextDeprecated
+          title={article?.title}
+          text={article?.subtitle}
+          size={TextSize.L}
+          data-testid={'Article'}
+        />
+
+        <HStack gap={'8'}>
+          <Icon Svg={EyeIcon} />
+          <TextDeprecated text={String(article?.views)} />
+        </HStack>
+
+        <HStack gap={'8'}>
+          <Icon Svg={CalendarIcon} />
+          <TextDeprecated text={article?.createdAt} />
+        </HStack>
+      </VStack>
+
+      {article?.blocks.map(renderArticleBlock)}
+    </>
+  );
+};
+
+const Redesigned = () => {
+  const article = useSelector(getArticleDetailsData);
+
+  return (
+    <>
+      <Text title={article?.title} size={'l'} bold />
+      <Text title={article?.subtitle} />
+      <AppImage
+        fallback={<Skeleton width={'100%'} height={420} border={'16'} />}
+        src={article?.img}
+        className={cls.img}
+      />
+
+      {article?.blocks.map(renderArticleBlock)}
+    </>
+  );
+};
+
 export const ArticleDetails = memo(({ className, id }: ArticleDetailsProps) => {
   const { t } = useTranslation('articles');
   const dispatch = useAppDispatch();
   const isLoading = useSelector(getArticleDetailsIsLoading);
-  const article = useSelector(getArticleDetailsData);
   const error = useSelector(getArticleDetailsError);
-
-  const renderBlock = useCallback((block: ArticleBlock) => {
-    switch (block.type) {
-      case ArticleBlockType.TEXT:
-        return <ArticleTextBlockComponent key={block.id} block={block} />;
-      case ArticleBlockType.IMAGE:
-        return <ArticleImageBlockComponent key={block.id} block={block} />;
-      case ArticleBlockType.CODE:
-        return <ArticleCodeBlockComponent key={block.id} block={block} />;
-      default:
-        return null;
-    }
-  }, []);
 
   useEffect(() => {
     if (__PROJECT__ !== 'storybook') {
@@ -72,48 +112,29 @@ export const ArticleDetails = memo(({ className, id }: ArticleDetailsProps) => {
   if (isLoading) {
     content = (
       <>
-        <Skeleton
+        <SkeletonDeprecated
           className={cls.avatar}
           width={200}
           height={200}
           border={'50%'}
         />
-        <Skeleton width={300} height={32} />
-        <Skeleton width={600} height={24} />
-        <Skeleton width={'100%'} height={200} />
-        <Skeleton width={'100%'} height={200} />
+        <SkeletonDeprecated width={300} height={32} />
+        <SkeletonDeprecated width={600} height={24} />
+        <SkeletonDeprecated width={'100%'} height={200} />
+        <SkeletonDeprecated width={'100%'} height={200} />
       </>
     );
   } else if (error) {
-    content = <Text title={t('errorHappened')} align={TextAlign.CENTER} />;
+    content = (
+      <TextDeprecated title={t('errorHappened')} align={TextAlign.CENTER} />
+    );
   } else {
     content = (
-      <>
-        <HStack justify={'center'} max>
-          <Avatar size={200} src={article?.img} className={cls.avatar} />
-        </HStack>
-
-        <VStack gap={'4'} max data-testid={'ArticleDetails.Info'}>
-          <Text
-            title={article?.title}
-            text={article?.subtitle}
-            size={TextSize.L}
-            data-testid={'Article'}
-          />
-
-          <HStack gap={'8'}>
-            <Icon Svg={EyeIcon} />
-            <Text text={String(article?.views)} />
-          </HStack>
-
-          <HStack gap={'8'}>
-            <Icon Svg={CalendarIcon} />
-            <Text text={article?.createdAt} />
-          </HStack>
-        </VStack>
-
-        {article?.blocks.map(renderBlock)}
-      </>
+      <ToggleFeatures
+        feature={'isAppRedesigned'}
+        on={<Redesigned />}
+        off={<Deprecated />}
+      />
     );
   }
 
--- src/entities/Article/ui/ArticleTextBlockComponent/ArticleTextBlockComponent.tsx
+++ src/entities/Article/ui/ArticleTextBlockComponent/ArticleTextBlockComponent.tsx
@@ -1,12 +1,13 @@
 import { memo } from 'react';
 
-
 import { ArticleTextBlock } from '../../model/types/article';
 
 import cls from './ArticleTextBlockComponent.module.scss';
 
 import { classNames } from '@/shared/lib/classNames/classNames';
-import { Text } from '@/shared/ui/deprecated/Text';
+import { ToggleFeatures } from '@/shared/lib/features';
+import { Text as TextDeprecated } from '@/shared/ui/deprecated/Text';
+import { Text } from '@/shared/ui/redesigned/Text';
 
 interface ArticleTextBlockComponentProps {
   className?: string;
@@ -19,9 +20,32 @@ export const ArticleTextBlockComponent = memo(
       <div
         className={classNames(cls.articleTextBlockComponent, {}, [className])}
       >
-        {block.title && <Text title={block.title} className={cls.title} />}
+        {block.title && (
+          <ToggleFeatures
+            feature={'isAppRedesigned'}
+            on={<Text title={block.title} className={cls.title} />}
+            off={<TextDeprecated title={block.title} className={cls.title} />}
+          />
+        )}
         {block.paragraphs.map(paragraph => (
-          <Text key={paragraph} text={paragraph} className={cls.paragraph} />
+          <ToggleFeatures
+            feature={'isAppRedesigned'}
+            key={paragraph}
+            on={
+              <Text
+                key={paragraph}
+                text={paragraph}
+                className={cls.paragraph}
+              />
+            }
+            off={
+              <TextDeprecated
+                key={paragraph}
+                text={paragraph}
+                className={cls.paragraph}
+              />
+            }
+          />
         ))}
       </div>
     );
